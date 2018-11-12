4efc0df2ca337ee47390b3ddd4f7e188b394b89f
Sravanth554 <sravanthpvn@gmail.com>
2018-11-11 22:52:00 -0600

    uploaded select all

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public interface ISravanthRepository<T>
+    {
+        List<T> GetAll();
+    }
+
+    public class Employee : ISravanthRepository<T>
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
